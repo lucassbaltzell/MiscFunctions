7e0c75b720e5c0ebc08bbd3f140d8a260f70e6db
Lucas Baltzell <lucassbaltzell@gmail.com>
2021-05-01 17:58:49 -0400

    Update FFTdelay.m

--- FFTdelay.m
+++ FFTdelay.m
@@ -1,18 +1,21 @@
 function y = FFTdelay(x,tau,fs)
 %returns a vector y which is x delayed by a factor T
+%OUTPUT
 %y: output time vector
+%INPUT
 %x: input time vector (mono)
 %tau: delay in seconds
 %fs: sample rate of x
 
-%created by Luke Baltzell 2021/04/20
+%created by Luke Baltzell, modified 04/20/2021
+
 [~,dim] = min(size(x));
 if dim == 1
     x = x';
 end
 
 N = length(x);
-f = [0:fs/N:fs/2]';
+f = [0:fs/N:floor(fs/2)]';
 
 X = fft(x);
 X(1:length(f)) = X(1:length(f)).*exp(-2*pi*1i*f*tau);
